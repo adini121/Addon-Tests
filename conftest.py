ca4a2f1f27203e5d87eee99d6e88d64d3ef4c91f
AdiMac <adamsken1221@gmail.com>
2016-01-19 09:50:50 +0100

    Phase 2 testing: modified to store sessionIDs

--- conftest.py
+++ conftest.py
@@ -6,8 +6,8 @@
 
 import py
 import pytest
-
-
+import MySQLdb
+from time import gmtime, strftime
 def pytest_runtest_setup(item):
     pytest_mozwebqa = py.test.config.pluginmanager.getplugin("mozwebqa")
     pytest_mozwebqa.TestSetup.services_base_url = item.config.option.services_base_url
@@ -46,3 +46,39 @@ def editable_user(stored_users):
 @pytest.fixture
 def paypal_user(variables):
     return variables['paypal']
+
+@pytest.fixture(autouse=True)
+def session_id(mozwebqa):
+    print 'Session ID: {}'.format(mozwebqa.selenium.session_id)
+    str = '{}\n'.format(mozwebqa.selenium.session_id)
+    str_session_id = '{}'.format(mozwebqa.selenium.session_id)
+
+    with open ("/home/nisal/python.txt", "a") as myfile:
+        myfile.write(str)
+
+    current_time = strftime("%Y-%m-%d %H:%M")
+    print('Current time is: {}'.format(current_time))
+    """ Connect to MySQL database """
+    try:
+        conn = MySQLdb.connect(host='localhost',
+                               user='root',
+                               passwd='',
+                               db='phase_two_amo_sids')
+
+        c = conn.cursor()
+        tblQuery = """CREATE TABLE IF NOT EXISTS test_session_ids (id int unsigned auto_increment not NULL,
+            session_id VARCHAR(60) not NULL,
+            date_created VARCHAR(100) not NULL,
+            primary key(id))"""
+        c.execute(tblQuery)
+        print('............Successfully created table .......')
+        insQuery = """insert into test_session_ids (session_id, date_created) values ('%s', '%s')"""
+        c = conn.cursor()
+        c.execute("insert into test_session_ids (session_id, date_created) values (%s, %s)", (str_session_id, current_time))
+        print('............Successfully ADDED to table .......')
+        conn.commit()
+    except:
+        print ('UNABLE TO PERFORM DATABASE OPERATION')
+
+    finally:
+        conn.close()
